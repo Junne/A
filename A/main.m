113f7d4774724def0c7ea47a8f6c654b707d1c94
Junne <junne.bai@gmail.com>
2016-12-14 17:46:26 +0800

    Initial Commit

---
+++ A/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  A
+//
+//  Created by baijf on 14/12/2016.
+//  Copyright © 2016 Junne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
